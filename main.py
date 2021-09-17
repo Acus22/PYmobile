d40b03e581e28ff2dca91c0bd63092baf774b2c7
Acus22 <90800719+Acus22@users.noreply.github.com>
2021-09-17 11:57:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print ('Hello world')
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MainApp(App):
+  def build(self):
+    return Button(text="Hello World")
+    
+MainApp().run()
